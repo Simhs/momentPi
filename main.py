419eb6ade03bd319aa384177f9a1291ed43190cc
Simhs <shs900701@gmail.com>
2017-05-18 03:09:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import thread
-
 from lib.core import Machine
 
-
 machine = Machine(480,320)
-
 while True:
     machine.displayFlip()
     cmd = machine.getCommand()
     machine.changeState(cmd)
-
-
