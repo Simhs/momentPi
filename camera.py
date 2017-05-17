eea450d7fd392ed30bc9037010aede4e9eabd845
gmPark01 <qra0428@gmail.com>
2017-05-17 12:25:11 +0000

    HyperLapseCam class
    startcapture(self, time, turns) 메소드에서 time : 찍을시간간격(초),  turns : 찍을횟수 를 입력받아 실행한다.
    
    총 찍을시간에 따른 찍을시간간격 계산이 필요하다.  자동으로 계산하도록 변경할 예정
    ex) 30초 영상을 만든다 가정했을 때, 900장의 사진을 찍어야하고(turns=900) 1시간 동안 찍는다면 3600초 / 900 장 이므로 4초에 한번씩찍도록 (time=4.0)으로 계산해야한다.

---
+++ camera.py
@@ -0,0 +1,20 @@
+import time
+import picamera
+
+class HyperLapseCam:
+
+	def startcapture(self, time, turns):
+		time = float(time)
+		turns = int(turns)
+		with picamera.PiCamera() as c:
+#			c.resolution = (2592,1944) 해상도 조절
+#			c.start_preview() preview시작
+			c.exposure_compensation=2
+			for i in range(turns):
+				c.capture('image'+str(i)+'.jpg')
+				time.sleep(time)
+				i = i + 1
+#			c.stop_preview() preview 끝
+
+if __name__ == "__main__":
+
