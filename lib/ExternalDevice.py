0c140c2e86569e041c45f71cce97079e715f258d
Simhs <shs900701@gmail.com>
2017-05-17 22:23:03 +0900

    Update ExternalDevice.py

--- lib/ExternalDevice.py
+++ lib/ExternalDevice.py
@@ -1,4 +1,18 @@
+
+import time
+import picamera
+
 class Dial:
   pass
+
 class HyperLapseCam:
-  pass
+	def startcapture(self, timer, turns):
+		timer = float(time)
+		turns = int(turns)
+		with picamera.PiCamera() as c:
+			# c.resolution = (2592,1944) 해상도 조절
+			c.exposure_compensation=2
+			for i in range(turns):
+				c.capture('image'+str(i)+'.jpg')
+				time.sleep(timer)
+				i = i + 1
