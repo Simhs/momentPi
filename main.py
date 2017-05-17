2b9b1108ad3dc5351b5166bcc3f99c73818df0a0
Simhs <shs900701@gmail.com>
2017-05-18 03:09:01 +0900

    메인 코드 커밋!
    끝이 보인다... 휴

---
+++ main.py
@@ -0,0 +1,13 @@
+import thread
+
+from lib.core import Machine
+
+
+machine = Machine(480,320)
+
+while True:
+    machine.displayFlip()
+    cmd = machine.getCommand()
+    machine.changeState(cmd)
+
+
