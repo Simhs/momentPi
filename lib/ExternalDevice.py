b1568cc03b5850947178ae1e66e64858c92fc29e
gmPark01 <qra0428@gmail.com>
2017-05-18 15:45:12 +0000

    총 시간만 입력하는 메소드

--- lib/ExternalDevice.py
+++ lib/ExternalDevice.py
@@ -6,6 +6,10 @@ class Dial:
   pass
 
 class HyperLapseCam:
+	def __init__(self,totaltime):
+		self.totaltime = float(totaltime)
+		pass
+	
 	def startcapture(self, timer, turns):
 		timer = float(time)
 		turns = int(turns)
@@ -15,6 +19,14 @@ class HyperLapseCam:
 			for i in range(turns):
 				c.capture('image'+str(i)+'.jpg')
 				time.sleep(timer)
+				
 	def test(self):
-		pass
+		timer = self.totaltime*3600/750
+		with picamera.PiCamera() as c:
+			c.resolution = (1920, 1080)
+			for i in range(750):
+				c.capture('image'+str(i)+'.jpg')
+				time.sleep(timer)
+				
+			
 	
