6324436deb4ef8db4aaa497cad36604d442cdef3
Dollar <shj4849@naver.com>
2017-05-20 16:51:51 +0900

    Add files via upload

--- lib/btn_poll.py
+++ lib/btn_poll.py
@@ -2,43 +2,36 @@ import RPi.GPIO as GPIO
 import time
 import sys
 import os
+class Button():
+        def __init__(self):
+                GPIO.setmode(GPIO.BCM)
+                GPIO.setup(19, GPIO.OUT)
+                GPIO.output(19, True)
+        def button(self):
+                state = 0
+                switch = 13
+                GPIO.setup(13,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+                
+        #GPIO.add_event_detect(24,GPIO.RISING, callback=handler)
 
-def button():
-	GPIO.setmode(GPIO.BCM)
-	state = 0
-	switch = 13
+                try:
+                        while True:
+                                if GPIO.input(switch) == 0:
+                                        while True:
+                                                if GPIO.input(switch) == 1:
+                                                        state = 1
+                                                        print 'change : ' + str(state)
+                                                        time.sleep(0.2)
+                                                        break
 
-	GPIO.setup(13,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#GPIO.add_event_detect(24,GPIO.RISING, callback=handler)
-
-	try:
-		while True:
-			if GPIO.input(switch) == 0:
-				while True:
-					if GPIO.input(switch) == 1:
-						state = 1
-						print 'change : ' + str(state)
-						time.sleep(0.2)
-						break
-
-				if state == 1:
-					#버튼 눌러졌을시 프린트기 인쇄
-					os.system('lpr -o fit-to-page /home/pi/image486.jpg')
-					print 'running~'
-				print 'check : ' + str(state)
-	finally:
-		print 'end'
+                                        if state == 1:
+                                                
+                                                os.system('lpr -o fit-to-page /home/pi/image486.jpg')
+                                                print 'running~'
+                                        print 'check : ' + str(state)
+                finally:
+                        print 'end'
 
 if __name__ == '__main__':
-	button()
-#def handler(channel):
-#	time.sleep(1)
-#	count = 1
-#	print count
-
-#GPIO.setup(24,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-#GPIO.add_event_detect(24,GPIO.RISING, callback=handler)
-#while True:
-#	time.sleep(0.5)
-#	count = 0
-#	print count
+	but = Button()
+	but.button()
