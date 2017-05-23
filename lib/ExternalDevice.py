f3ca8fd5af7c94b60fcae81d9ca50c126ca18051
gmPark01 <qra0428@gmail.com>
2017-05-23 12:50:39 +0000

    Add files via upload

--- lib/ExternalDevice.py
+++ lib/ExternalDevice.py
@@ -1,16 +1,108 @@
-
+#-*- coding: utf-8 -*-
 import time
 import picamera
+import RPi.GPIO as GPIO
+import sys
+import os
+
+class Button:    
+    def __init__(self):
+        self.button_state = 0
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(19, GPIO.OUT)
+        GPIO.output(19, True)
+
+    def isPushed(self):
+        print "test success"
+        switch = 13
+        GPIO.setup(13,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        try:
+            while True:
+                if GPIO.input(switch) == 0:
+                    while True:
+                        if GPIO.input(switch) == 1:
+                            self.button_state = 1
+                            time.sleep(0.2)
+                            print self.button_state
+                            break
+                break
+
+        finally:
+            print 'end'
+
+        return self.button_state
+
 
 class Dial:
-  pass
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        # SPI port on the ADC to the Cobbler
+        self.SPICLK = 12
+        self.SPIMISO = 16
+        self.SPIMOSI = 20
+        self.SPICS = 21
+        self.POWER = 26
+
+        # set up the SPI interface pins
+        GPIO.setup(self.POWER, GPIO.OUT)
+        GPIO.setup(self.SPIMOSI, GPIO.OUT)
+        GPIO.setup(self.SPIMISO, GPIO.IN)
+        GPIO.setup(self.SPICLK, GPIO.OUT)
+        GPIO.setup(self.SPICS, GPIO.OUT)
+        GPIO.output(self.POWER, True)
+
+        # 10k trim pot connected to adc #0
+        self.potentiometer_adc = 0;
+        
+    def readadc(self, adcnum, clockpin, mosipin, misopin, cspin):
+        if ((adcnum > 7) or (adcnum < 0)):
+            return -1
+        GPIO.output(cspin, True)
+        
+        GPIO.output(clockpin, False)  # start clock low
+        GPIO.output(cspin, False)     # bring CS low
+        
+        commandout = adcnum
+        commandout |= 0x18  # start bit + single-ended bit
+        commandout <<= 3    # we only need to send 5 bits here
+        for i in range(5):
+            if (commandout & 0x80):
+                GPIO.output(mosipin, True)
+            else:
+                GPIO.output(mosipin, False)
+            commandout <<= 1
+            GPIO.output(clockpin, True)
+            GPIO.output(clockpin, False)
+        
+        adcout = 0
+        # read in one empty bit, one null bit and 10 ADC bits
+        for i in range(12):
+            GPIO.output(clockpin, True)
+            GPIO.output(clockpin, False)
+            adcout <<= 1
+            if (GPIO.input(misopin)):
+                adcout |= 0x1
+        
+        GPIO.output(cspin, True)
+        adcout >>= 1       # first bit is 'null' so drop it
+                
+        return adcout
+
+    def getValue(self):
+        trim_pot = self.readadc(self.potentiometer_adc, self.SPICLK, self.SPIMOSI, self.SPIMISO, self.SPICS)
+        if(trim_pot != 0):
+            set_vel = int(round(trim_pot*705/1023))+15
+        else:
+            set_vel = 0
+        return set_vel
+
 
 class HyperLapseCam:
 	def __init__(self,totaltime):
 		self.totaltime = float(totaltime)
 		pass
 	
-	def startcapture(self, timer, turns):
+	def startCapture(self, timer, turns):
 		timer = float(time)
 		turns = int(turns)
 		with picamera.PiCamera() as c:
@@ -27,7 +119,4 @@ class HyperLapseCam:
 			c.resolution = (1920, 1080)
 			for i in range(750):
 				c.capture('image'+str(i)+'.jpg')
-				time.sleep(timer)
-				
-			
-	
+				time.sleep(timer)	
